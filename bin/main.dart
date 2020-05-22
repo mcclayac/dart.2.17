0176493882a50601e362ec0dae3c8b494ea364a2
mcclayac <anthony.mcclay@gmail.com>
2020-05-22 00:52:02 -0400

    Added comments

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,10 @@
 //code 2.17
 main(List<String> arguments) {
+  // declarations
   List fruitCollection = ['Mango', 'Apple', 'Jack fruit'];
   var myIntegers = [1, 2, 3, 'non-integer object'];
+
+  //  logic
   print(myIntegers[3]);
   print(fruitCollection[0]);
 }
