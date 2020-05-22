d6defcb436bff41bd6e08225df836ab636520f10
mcclayac <anthony.mcclay@gmail.com>
2020-05-22 00:51:11 -0400

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,7 @@
+//code 2.17
+main(List<String> arguments) {
+  List fruitCollection = ['Mango', 'Apple', 'Jack fruit'];
+  var myIntegers = [1, 2, 3, 'non-integer object'];
+  print(myIntegers[3]);
+  print(fruitCollection[0]);
+}
